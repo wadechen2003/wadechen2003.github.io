c5a970305a280a9efad7340a85fb3ebe3260eb49
wadechen <wadechen2003@gmail.com>
2026-07-13 20:41:10 +0800

    Add Firebase config and security rules for GL Live Tracker

---
+++ tools/gl-live-tracker/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBAI0AiZvdLEXVKuYfkC8QesUdqYFRwdNE",
+  authDomain: "gl-points-in-game.firebaseapp.com",
+  databaseURL: "https://gl-points-in-game-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gl-points-in-game",
+  storageBucket: "gl-points-in-game.firebasestorage.app",
+  messagingSenderId: "582834631732",
+  appId: "1:582834631732:web:3b3fa7db0c517d2ff64661",
+  measurementId: "G-7M3B6TC44G"
+};
